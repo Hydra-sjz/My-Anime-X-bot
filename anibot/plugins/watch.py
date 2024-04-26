b945e98ee8596212d8ca49cb022dcfa910cacc2f
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-26 09:57:25 +0530

    Update watch.py

--- anibot/plugins/watch.py
+++ anibot/plugins/watch.py
@@ -25,24 +25,24 @@ DC = get_collection('DISABLED_CMDS')
 )
 @control_user
 async def get_watch_order(client: Client, message: Message, mdata: dict):
-    """Get List of Scheduled Anime"""
+    """__📜Get List of Scheduled Anime📜__"""
     gid = mdata['chat']['id']
     find_gc = await DC.find_one({'_id': gid})
     if find_gc is not None and 'watch' in find_gc['cmd_list'].split():
         return
     x = message.text.split(" ", 1)
     if len(x)==1:
-        await message.reply_text("Nothing given to search for!!!")
+        await message.reply_text("😕 __Nothing given to search for!!!__")
         return
     try:
         user = mdata['from_user']['id']
     except KeyError:
         user = mdata['sender_chat']['id']
     data = get_wols(x[1])
-    msg = f"Found related animes for the query {x[1]}"
+    msg = f"__🍃 Found related animes for the query {x[1]}__\n\n©️ @XBOTS_X"
     buttons = []
     if data == []:
-        await client.send_message(gid, 'No results found!!!')
+        await client.send_message(gid, '❌ No results found!!!')
         return
     for i in data:
         buttons.append(
@@ -72,7 +72,7 @@ async def watch_(client: anibot, cq: CallbackQuery, cdata: dict):
             button.append(
                 [
                     InlineKeyboardButton(
-                        text="Next",
+                        text="Next ➡️",
                         callback_data=f"{kek}_{id_}_{qry}_{int(req)+1}_{user}"
                     )
                 ]
@@ -81,7 +81,7 @@ async def watch_(client: anibot, cq: CallbackQuery, cdata: dict):
             button.append(
                 [
                     InlineKeyboardButton(
-                        text="Prev",
+                        text="⬅️ Prev",
                         callback_data=f"{kek}_{id_}_{qry}_{int(req)-1}_{user}"
                     )
                 ]
@@ -90,17 +90,17 @@ async def watch_(client: anibot, cq: CallbackQuery, cdata: dict):
             button.append(
                 [
                     InlineKeyboardButton(
-                        text="Prev",
+                        text="⬅️ Prev",
                         callback_data=f"{kek}_{id_}_{qry}_{int(req)-1}_{user}"
                     ),
                     InlineKeyboardButton(
-                        text="Next",
+                        text="Next ➡️",
                         callback_data=f"{kek}_{id_}_{qry}_{int(req)+1}_{user}"
                     )
                 ]
             )
     button.append([
-        InlineKeyboardButton("Back", callback_data=f"wol_{qry}_{user}")
+        InlineKeyboardButton("◀️ Back", callback_data=f"wol_{qry}_{user}")
     ])
     await cq.edit_message_text(msg, reply_markup=InlineKeyboardMarkup(button))
 
@@ -110,7 +110,7 @@ async def watch_(client: anibot, cq: CallbackQuery, cdata: dict):
 async def wls(client: anibot, cq: CallbackQuery, cdata: dict):
     kek, qry, user = cdata['data'].split("_")
     data = get_wols(qry)
-    msg = f"Found related animes for the query {qry}"
+    msg = f"🍃 __Found related animes for the query {qry}__\n\n©️ @XBOTS_X"
     buttons = []
     for i in data:
         buttons.append(
@@ -125,7 +125,7 @@ async def wls(client: anibot, cq: CallbackQuery, cdata: dict):
 
 
 @anibot.on_edited_message(
-    filters.command(["watch", f"watch{BOT_NAME}"], prefixes=trg)
+    filters.command(["📺 Watch", f"watch{BOT_NAME}"], prefixes=trg)
 )
 async def get_watch_order_edit(client: Client, message: Message):
     await get_watch_order(client, message)
