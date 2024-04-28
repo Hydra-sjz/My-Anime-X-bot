565f61c3dd163505caedd7e7906398ce8fc3012c
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-28 16:52:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,11 @@ SUDO_USERS = environ.get("SUDO_USERS", str(OWNER_ID)).split()
 #MOMGO_DATABASE
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "")
+
+#API_TOOLS
+CURRENCY_API = environ.get("CURRENCY_API")
+GPT_API = os.environ.get("GPT_API")
+DAXX_API = os.environ.get("DAXX_API")
+DEEP_API = os.environ.get("DEEP_API")
+UNSPLASH_ACCESS_KEY = os.getenv('UNSPLASH_ACCESS_KEY','')
+GENIUS_API = os.getenv('GENIUS_API')
