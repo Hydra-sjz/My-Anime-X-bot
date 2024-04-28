12d95f419254416057a4ac31c0a2d468c7c803cd
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-28 13:44:23 +0530

    Update and rename config to config.py

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ from os import environ, mkdir, path, sys
 
 OWNER_ID = int(environ["OWNER_ID"])
 SUDO_USERS = environ.get("SUDO_USERS", str(OWNER_ID)).split()
+
+#MOMGO_DATABASE
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "")
