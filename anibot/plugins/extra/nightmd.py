51e9a1a4c7d4ba3ab8f1463306af44b65608f187
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-29 00:17:17 +0530

    Update nightmd.py

--- anibot/plugins/extra/nightmd.py
+++ anibot/plugins/extra/nightmd.py
@@ -81,7 +81,7 @@ async def start_nightmode() :
             print(f"[bold red] Unable To close Group {add_chat} - {e}")
 
 scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
-scheduler.add_job(start_nightmode, trigger="cron", hour=00, minute=10) #hour=23, minute=59
+scheduler.add_job(start_nightmode, trigger="cron", hour=00, minute=20) #hour=23, minute=59
 scheduler.start()
 
 async def close_nightmode():
