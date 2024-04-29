600640edba9bc10c0683b33be39eb27bcda9bafe
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-29 22:48:18 +0530

    Update check_user.py

--- anibot/utils2/broadcast_db/check_user.py
+++ anibot/utils2/broadcast_db/check_user.py
@@ -1,7 +1,7 @@
 import datetime
 from config import DB_URL, DB_NAME, LOG_CHANNEL
 import logging
-from mbot.utils.broadcast_db.database import Database
+from anibot.utils2.broadcast_db.database import Database
 
 db = Database(DB_URL, DB_NAME)
 async def handle_user_status(bot, cmd):
