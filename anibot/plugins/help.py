31a217967cb562f0552f3e56b2239a412366e1dd
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-29 22:51:48 +0530

    Update help.py

--- anibot/plugins/help.py
+++ anibot/plugins/help.py
@@ -1,9 +1,23 @@
+import os
+import asyncio
+import traceback
+import logging
+
 from pyrogram import filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from anibot import anibot, LOG_CHANNEL_ID
 import wget
 import requests as re
 from pyrogram import *
+
+from anibot.utils2.broadcast_db.broadcast import broadcast
+from anibot.utils2.broadcast_db.check_user import handle_user_status
+from anibot.utils2.broadcast_db.database import Database
+from config import AUTH_USERS, DB_URL, DB_NAME
+
+
+db = Database(DB_URL, DB_NAME)
+
 SPO = """
 ➡️ **☠️LOG STURO☠️** ⬅️
 
@@ -74,6 +88,44 @@ Apart from above shown cmds__
 
 ©️ @XBOTS_X
 """
+
+start_cmd = """
+Hello {} Welcome to Gojo Satoru 𝕏 Bot
+"""
+startbt = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton('📣 My Channel', url='https://t.me/XBots_X')
+            ],[
+                InlineKeyboardButton("Only for Owner", callback_data="own")
+            ]
+        ]
+)
+
+
+
+@Mbot.on_message(filters.private)
+async def _(bot, cmd):
+    await handle_user_status(bot, cmd)
+@Mbot.on_message(filters.private & filters.command("skw"))
+async def starwkommand(bot, message):
+    chat_id = message.from_user.id
+    if not await db.is_user_exist(chat_id):
+        data = await client.get_me()
+        await db.add_user(chat_id)
+        if LOG_CHANNEL:
+            await client.send_message(
+                LOG_CHANNEL,
+                f"🥳NEWUSER🥳 \n\n😼New User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) 😹started @spotifysavetgbot !!",
+            )
+        else:
+            logging.info(f"🥳NewUser🥳 :- 😼Name : {message.from_user.first_name} 😹ID : {message.from_user.id}")
+    await message.reply_text(
+        text=start_cmd.format(message.from_user.first_name), 
+        reply_markup=startbt,
+    )
+    await message.delete()
+
 @anibot.on_message(filters.private & filters.command("help"))
 async def hlp_cmd(bot, message):
     await bot.send_message(LOG_CHANNEL_ID, SPO.format(message.from_user.mention, message.from_user.username, message.from_user.dc_id, message.from_user.id))
@@ -398,3 +450,124 @@ EXT2_BUTTONS = InlineKeyboardMarkup(
         InlineKeyboardButton("❌ Cancel", callback_data="close")
         ]]
     ) 
+
+#==================•BROADCAST•==================
+@Mbot.on_message(filters.private & filters.command(["broadcast", "send"]))
+async def broadcast_handler_open(_, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    if m.reply_to_message is None:
+        await m.delete()
+    else:
+        await broadcast(m, db)
+
+@Mbot.on_message(filters.private & filters.command("stat"))
+async def tsts(c, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    sat = await m.reply_text(
+        text=f"**Total Users in Database 📂:** `{await db.total_users_count()}`\n\n**Total Users with Notification Enabled 🔔 :** `{await db.total_notif_users_count()}`",
+        quote=True
+    )
+    await m.delete()
+    await asyncio.sleep(180)
+    await sat.delete()
+
+@Mbot.on_message(filters.private & filters.command("ban_user"))
+async def ban(c, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    if len(m.command) == 1:
+        await m.reply_text(
+            f"Use this command to ban 🛑 any user from the bot 🤖.\n\nUsage:\n\n`/ban_user user_id ban_duration ban_reason`\n\nEg: `/ban_user 1234567 28 You misused me.`\n This will ban user with id `1234567` for `28` days for the reason `You misused me`.",
+            quote=True,
+        )
+        return
+
+    try:
+        user_id = int(m.command[1])
+        ban_duration = int(m.command[2])
+        ban_reason = " ".join(m.command[3:])
+        ban_log_text = f"Banning user {user_id} for {ban_duration} days for the reason {ban_reason}."
+
+        try:
+            await c.send_message(
+                user_id,
+                f"You are Banned 🚫 to use this bot for **{ban_duration}** day(s) for the reason __{ban_reason}__ \n\n**Message from the admin 🤠**",
+            )
+            ban_log_text += "\n\nUser notified successfully!"
+        except BaseException:
+            traceback.print_exc()
+            ban_log_text += (
+                f"\n\n ⚠️ User notification failed! ⚠️ \n\n`{traceback.format_exc()}`"
+            )
+        await db.ban_user(user_id, ban_duration, ban_reason)
+        print(ban_log_text)
+        await m.reply_text(ban_log_text, quote=True)
+    except BaseException:
+        traceback.print_exc()
+        await m.reply_text(
+            f"Error occoured ⚠️! Traceback given below\n\n`{traceback.format_exc()}`",
+            quote=True
+        )
+
+@Mbot.on_message(filters.private & filters.command("unban_user"))
+async def unban(c, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    if len(m.command) == 1:
+        await m.reply_text(
+            f"Use this command to unban 😃 any user.\n\nUsage:\n\n`/unban_user user_id`\n\nEg: `/unban_user 1234567`\n This will unban user with id `1234567`.",
+            quote=True,
+        )
+        return
+
+    try:
+        user_id = int(m.command[1])
+        unban_log_text = f"Unbanning user 🤪 {user_id}"
+
+        try:
+            await c.send_message(user_id, f"Your ban was lifted!")
+            unban_log_text += "\n\n✅ User notified successfully! ✅"
+        except BaseException:
+            traceback.print_exc()
+            unban_log_text += (
+                f"\n\n⚠️ User notification failed! ⚠️\n\n`{traceback.format_exc()}`"
+            )
+        await db.remove_ban(user_id)
+        print(unban_log_text)
+        await m.reply_text(unban_log_text, quote=True)
+    except BaseException:
+        traceback.print_exc()
+        await m.reply_text(
+            f"⚠️ Error occoured ⚠️! Traceback given below\n\n`{traceback.format_exc()}`",
+            quote=True,
+        )
+
+@Mbot.on_message(filters.private & filters.command("banned_users"))
+async def banned_usrs(c, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    all_banned_users = await db.get_all_banned_users()
+    banned_usr_count = 0
+    text = ""
+    async for banned_user in all_banned_users:
+        user_id = banned_user["id"]
+        ban_duration = banned_user["ban_status"]["ban_duration"]
+        banned_on = banned_user["ban_status"]["banned_on"]
+        ban_reason = banned_user["ban_status"]["ban_reason"]
+        banned_usr_count += 1
+        text += f"🆔**User_id** : `{user_id}`\n⏱️**Ban Duration** : `{ban_duration}`\n\n📆**Banned on** : `{banned_on}`\n\n💁**Reason**: `{ban_reason}`\n\n😌 @Musicx_dlbot"
+    reply_text = f"Total banned user(s) 🤭: `{banned_usr_count}`\n\n{text}"
+    if len(reply_text) > 4096:
+        with open("banned-users.txt", "w") as f:
+            f.write(reply_text)
+        await m.reply_document("banned-users.txt", True)
+        os.remove("banned-users.txt")
+        return
+    await m.reply_text(reply_text, True)
