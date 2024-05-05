c1adbaad819fba726c3af410f563508b7bb0efa6
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-05-05 14:35:30 +0530

    Update qr.py

--- anibot/plugins/extra/qr.py
+++ anibot/plugins/extra/qr.py
@@ -1,10 +1,46 @@
 import os
 import cv2
 import qrcode
+from PIL import Image
+import io
 from pyrogram import Client, filters
 
+from pyrogram.types import Message
 
-@Client.on_message(filters.command('qr'))
+
+
+#TEXT TO QR CODE
+def generate_qr_code(text):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="white", back_color="black")
+
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format='PNG')
+    img_bytes.seek(0)
+
+    return img_bytes
+
+@Client.on_message(filters.command("txt_qr"))
+def qr_handler(client, message: Message):
+    command_text = message.command
+    if len(command_text) > 1:
+        input_text = " ".join(command_text[1:])
+        qr_image = generate_qr_code(input_text)
+        message.reply_photo(qr_image, caption="Here's your QR Code")
+    else:
+        message.reply_text("Please provide the text for the QR code after the command. Example usage: /qr text")
+
+
+#QR_CODE TO TEXT
+@Client.on_message(filters.command('qr_txt'))
 async def qr(c, m):
     if " " in m.text:
         tdl = await m.reply_text("`Please wait...`")
