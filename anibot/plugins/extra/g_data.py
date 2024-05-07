4346f582e4634a3300e540645ba6c6862bfb204f
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-05-07 20:40:22 +0530

    Update g_data.py

--- anibot/plugins/extra/g_data.py
+++ anibot/plugins/extra/g_data.py
@@ -6,7 +6,7 @@ from pyrogram import enums
 
 from anibot import anibot as app
 
-@app.on_message(~filters.private & filters.command(["gimfo"]), group=2)
+@app.on_message(~filters.private & filters.command(["ginfo"]), group=2)
 async def instatus(app, message):
     start_time = time.perf_counter()
     user = await app.get_chat_member(message.chat.id, message.from_user.id)
