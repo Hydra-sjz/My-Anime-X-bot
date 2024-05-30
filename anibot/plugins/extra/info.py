3c1a1c0a72331bf4193290446883f0a23dce7123
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-05-30 09:32:14 +0530

    Update info.py

--- anibot/plugins/extra/info.py
+++ anibot/plugins/extra/info.py
@@ -4,6 +4,189 @@ import pyrogram
 from pyrogram import Client, filters
 from anibot import anibot as app
 
+
+
+import asyncio, os, time, aiohttp
+from pathlib import Path
+from PIL import Image, ImageDraw, ImageFont
+from asyncio import sleep
+from pyrogram import filters, Client, enums
+from pyrogram.enums import ParseMode
+from pyrogram.types import *
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton
+from typing import Union, Optional
+
+EVAA = [
+    [
+        InlineKeyboardButton(text="Add me to group", url=f"https://t.me/GojoSatoru_Xbot?startgroup=true"),
+    ],
+]
+
+get_font = lambda font_size, font_path: ImageFont.truetype(font_path, font_size)
+resize_text = (
+    lambda text_size, text: (text[:text_size] + "...").upper()
+    if len(text) > text_size
+    else text.upper()
+)
+
+# --------------------------------------------------------------------------------- #
+
+
+async def get_userinfo_img(
+    bg_path: str,
+    font_path: str,
+    user_id: Union[int, str],    
+    profile_path: Optional[str] = None
+):
+    bg = Image.open(bg_path)
+
+    if profile_path:
+        img = Image.open(profile_path)
+        mask = Image.new("L", img.size, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.pieslice([(0, 0), img.size], 0, 360, fill=255)
+
+        circular_img = Image.new("RGBA", img.size, (0, 0, 0, 0))
+        circular_img.paste(img, (0, 0), mask)
+        resized = circular_img.resize((534, 534))
+        bg.paste(resized, (607, 86), resized)
+
+    img_draw = ImageDraw.Draw(bg)
+
+    
+
+    path = f"./userinfo_img_{user_id}.png"
+    bg.save(path)
+    return path
+   
+
+# --------------------------------------------------------------------------------- #
+
+bg_path = "assets/IN.PNG"
+font_path = "assets/hiroko.ttf"
+
+#
+# --------------------------------------------------------------------------------- #
+
+
+INFO_TEXT = """
+ㅤ× User Information ×
+×───────────────×
+  
+× User Id: `{}`
+× Username: @{}
+× Mention: {}
+× Status: `{}`
+× Dc Id: {}
+× Bio: {}
+
+๏ Powered by: [X Bots](https://t.me/xbots_x)
+"""
+
+# --------------------------------------------------------------------------------- #
+
+async def userstatus(user_id):
+   try:
+      user = await app.get_users(user_id)
+      x = user.status
+      if x == enums.UserStatus.RECENTLY:
+         return "User was seen recently."
+      elif x == enums.UserStatus.LAST_WEEK:
+          return "User was seen last week."
+      elif x == enums.UserStatus.LONG_AGO:
+          return "User was seen long ago."
+      elif x == enums.UserStatus.OFFLINE:
+          return "User is offline."
+      elif x == enums.UserStatus.ONLINE:
+         return "User is online."
+   except:
+        return "**Something went wrong!**"
+    
+
+# --------------------------------------------------------------------------------- #
+
+@app.on_message(filters.command(["info", "information", "userinfo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
+async def userinfo(_, message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    
+    if not message.reply_to_message and len(message.command) == 2:
+        try:
+            user_id = message.text.split(None, 1)[1]
+            user_info = await app.get_chat(user_id)
+            user = await app.get_users(user_id)
+            status = await userstatus(user.id)
+            id = user_info.id
+            dc_id = user.dc_id
+            name = user_info.first_name
+            username = user_info.username
+            mention = user.mention
+            bio = user_info.bio
+            photo = await app.download_media(user.photo.big_file_id)
+            welcome_photo = await get_userinfo_img(
+                bg_path=bg_path,
+                font_path=font_path,
+                user_id=user_id,
+                profile_path=photo,
+            )
+            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+                id, username, mention, status, dc_id, bio), reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(EVAA),)
+        except Exception as e:
+            await message.reply_text(str(e))        
+      
+    elif not message.reply_to_message:
+        try:
+            user_info = await app.get_chat(user_id)
+            user = await app.get_users(user_id)
+            status = await userstatus(user.id)
+            id = user_info.id
+            dc_id = user.dc_id
+            name = user_info.first_name
+            username = user_info.username
+            mention = user.mention
+            bio = user_info.bio
+            photo = await app.download_media(user.photo.big_file_id)
+            welcome_photo = await get_userinfo_img(
+                bg_path=bg_path,
+                font_path=font_path,
+                user_id=user_id,
+                profile_path=photo,
+            )
+            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+                id, username, mention, status, dc_id, bio), reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(EVAA),)
+        except Exception as e:
+            await message.reply_text(str(e))
+
+            
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        try:
+            user_info = await app.get_chat(user_id)
+            user = await app.get_users(user_id)
+            status = await userstatus(user.id)
+            id = user_info.id
+            dc_id = user.dc_id
+            name = user_info.first_name
+            username = user_info.username
+            mention = user.mention
+            bio = user_info.bio
+            photo = await app.download_media(message.reply_to_message.from_user.photo.big_file_id)
+            welcome_photo = await get_userinfo_img(
+                bg_path=bg_path,
+                font_path=font_path,
+                user_id=user_id,
+                profile_path=photo,
+            )
+            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+                id, username, mention, status, dc_id, bio), reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(EVAA),)
+        except Exception as e:
+            await message.reply_text(str(e))
+
+####
+
+
+
+
 bullets = {
     "bullet1": ">",
     "bullet2": "•",
@@ -31,13 +214,13 @@ dc_id = {
 }
 
 
-@Client.on_message(filters.command(["info", "whois"]))
+@Client.on_message(filters.command(["2info", "whois"]))
 async def whois(bot, message):
     global chat
     msg = await message.reply_text("`Processing...`")
     if message.reply_to_message:
         user = message.reply_to_message.from_user.id
-    elif len(message.text.split(" ", 1)) == 1 and not message.reply_to_message:
+    elif len(message.text.split(" ", 1)) == 1 and not message.reply_to_mssage:
         user = message.from_user.id
         chat = message.chat.id
         t = "Chat id: {}".format(chat)
