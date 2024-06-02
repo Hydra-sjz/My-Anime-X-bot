4a241009e3914f3f91835b6130d4f3c4a3d9d792
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-06-02 08:04:38 +0530

    Update whisper.py

--- anibot/plugins/extra/whisper.py
+++ anibot/plugins/extra/whisper.py
@@ -43,7 +43,8 @@ async def _whisper(_, inline_query):
                         )
                     )]]
                 )
-            )],         
+            )
+        ]
         
     else:
         try:
