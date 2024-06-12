25771dbab79283da01ae3d8e648d2ffc463b96da
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-06-12 23:55:06 +0530

    Create functions.py

---
+++ anibot/utils2/help/functions.py
@@ -0,0 +1,15 @@
+from re import findall, search
+from anibot import aiohttpsession as aiosession
+
+
+async def get_http_status_code(url: str) -> int:
+    async with aiosession.head(url) as resp:
+        return resp.status
+
+
+def get_urls_from_text(text: str) -> bool:
+    regex = r"""(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]
+                [.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(
+                \([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\
+                ()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""".strip()
+    return [x[0] for x in findall(regex, str(text))]
