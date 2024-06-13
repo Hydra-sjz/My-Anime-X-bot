3092acbfbd82ee41ed931affbe48074661f1be2f
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-06-13 08:12:48 +0530

    Update rssdb.py

--- anibot/utils2/help/rssdb.py
+++ anibot/utils2/help/rssdb.py
@@ -42,9 +42,11 @@ async def add_rss_feed(chat_id: int, url: str, last_title: str):
         {"$set": {"url": url, "last_title": last_title}},
         upsert=True,
     )
+    
 
 async def remove_rss_feed(chat_id: int):
     return await rssdb.delete_one({"chat_id": chat_id})
+    
 
 async def update_rss_feed(chat_id: int, last_title: str):
     return await rssdb.update_one(
@@ -52,9 +54,12 @@ async def update_rss_feed(chat_id: int, last_title: str):
         {"$set": {"last_title": last_title}},
         upsert=True,
     )
+    
 
 async def is_rss_active(chat_id: int) -> bool:    
     return rssdb.find_one({"chat_id": chat_id})
+
+
 async def get_rss_feeds() -> list:
     data = []
     async for feed in rssdb.find({"chat_id": {"$exists": 1}}):
