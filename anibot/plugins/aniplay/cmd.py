8275798f73037b7f7922a600d6d923a879a86769
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-07-14 21:36:47 +0530

    Create cmd.py

---
+++ anibot/plugins/aniplay/cmd.py
@@ -0,0 +1,53 @@
+from pyrogram.types import Message
+from pyrogram import filters
+from AniPlay import app
+from anibot.plugins.animedex import AnimeDex
+from anibot.plugins.button import BTN
+from anibot.plugins.stats import day, over
+
+
+
+QUERY = '**Search Results:** `{}`'
+
+
+@app.on_message(filters.command(['search', 's']))
+async def searchCMD(_, message: Message):
+    try:
+        user = message.from_user.id
+        query = ' '.join(message.command[1:])
+        if query == '':
+            return await message.reply_text('Give me something to search ^_^')
+        data = AnimeDex.search(query)
+        button = BTN.searchCMD(user, data, query)
+        await message.reply_text(QUERY.format(query), reply_markup=button)
+    except Exception as e:
+        try:
+            return await message.reply_text('**Anime Not Found...**\n\nProbably Incorrect Name, Try again')
+        except:
+            return
+
+
+@app.on_message(filters.command('stats'))
+async def stats(_, message: Message):
+    try:
+        await message.reply_text('Use /stats1 For Day Wise Stats\nAnd /stats2 For Overall Stats')
+    except:
+        return
+
+
+@app.on_message(filters.command('stats1'))
+async def stats1(_, message: Message):
+    try:
+        img = day()
+        await message.reply_photo(img, caption='**AnimeDex | Day Wise Stats**')
+    except:
+        return
+
+
+@app.on_message(filters.command('stats2'))
+async def stats2(_, message: Message):
+    try:
+        img = over()
+        await message.reply_photo(img, caption='**AnimeDex | Overall Stats**')
+    except:
+        return
